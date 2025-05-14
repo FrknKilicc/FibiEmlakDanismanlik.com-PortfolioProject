ec714e1b1d7b49cfb70cf6e3d1b292a129ac5a13
Furkan Kılıç <youkali40@gmail.com>
2025-05-14 23:01:58 +0300

    - Created a new partial view to render the agent list dynamically. - Integrated the agent list into the main agent page. - Added header and footer as reusable partial views for layout consistency. - Ensured proper synchronization between layout and partial components. - Applied CSS styles to maintain visual consistency with the rest of the site.

---
+++ Frontends/FibiEmlakDanismanlik.WebUI/ViewComponents/AgentViewComponents/_GetAgentListPageViewComponentPartial.cs
@@ -0,0 +1,33 @@
+﻿using FibiEmlakDanismanlik.Dto.AgentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FibiEmlakDanismanlik.WebUI.ViewComponents.AgentViewComponents
+{
+    public class _GetAgentListPageViewComponentPartial : ViewComponent
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _GetAgentListPageViewComponentPartial(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client =  _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"{_configuration["Url:ApiUrl"]}Agent");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultAgentDto>>(jsonData);
+                return View(value);
+            }
+
+
+            return View(null);
+        }
+    }
+}
