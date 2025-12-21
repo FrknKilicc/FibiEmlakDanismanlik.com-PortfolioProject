79749260274a92f1d10088103120187f1f145e50
Furkan Kılıç <youkali40@gmail.com>
2025-12-21 20:53:28 +0300

    Added blog search suggestion feature with dynamic results Live suggestions are shown based on typed keywords

---
+++ Frontends/FibiEmlakDanismanlik.Dto/BlogDtos/BlogSuggestionDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibiEmlakDanismanlik.Dto.BlogDtos
+{
+    public class BlogSuggestionDto
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogDescPrew { get; set; }
+        public string BlogImgUrl { get; set; }
+    }
+}
--- Frontends/FibiEmlakDanismanlik.WebUI/Controllers/BlogController.cs
+++ Frontends/FibiEmlakDanismanlik.WebUI/Controllers/BlogController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FibiEmlakDanismanlik.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace FibiEmlakDanismanlik.WebUI.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public BlogController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -14,6 +26,28 @@ namespace FibiEmlakDanismanlik.WebUI.Controllers
             ViewBag.BlogId = id;    
             return View();
 
+        }
+        [HttpGet]
+        public async Task<IActionResult> Suggest(string q, int take = 6)
+        {
+            if(string.IsNullOrWhiteSpace(q)|| q.Trim().Length < 2)
+            {
+                return Json(new List<BlogSuggestionDto>());
+            }
+            if(take<=0) take = 6;
+            if(take>10) take = 10;
+
+            var client = _httpClientFactory.CreateClient();
+            var apiUrl = _configuration["Url:ApiUrl"];
+            var url = $"{apiUrl}Blog/SearchSuggestions?q={Uri.EscapeDataString(q.Trim())}&take={take}";
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                return Json(new List<BlogSuggestionDto>());
+            }
+            var json = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<List<BlogSuggestionDto>>(json) ?? new ();
+            return Json(data);
         }
 
     }
